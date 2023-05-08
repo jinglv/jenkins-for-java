ecf2e61d8e64871328de1dcff90f1b36eef8072d
JeanLv <7930695+jeanlv@user.noreply.gitee.com>
2023-05-08 10:13:49 +0800

    [fetch]修改Jenkins执行名传参方式

--- src/test/java/com/run/demo/utils/JenkinsClientTest.java
+++ src/test/java/com/run/demo/utils/JenkinsClientTest.java
@@ -1,9 +1,11 @@
 package com.run.demo.utils;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.run.demo.entity.JenkinsInfo;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -13,17 +15,22 @@ import java.util.Map;
 class JenkinsClientTest {
 
     @Test
-    void operateJenkinsJob() {
+    void operateJenkinsJob() throws IOException {
         JenkinsInfo jenkinsInfo = new JenkinsInfo();
         jenkinsInfo.setJenkinsBaseUrl("http://8.140.112.109:8080/jenkins/");
         jenkinsInfo.setUsername("admin");
         jenkinsInfo.setPassword("admin");
         jenkinsInfo.setJenkinsJobName("Test_Job");
         jenkinsInfo.setJenkinsBaseXml("base_jenkins");
-        Map<String, String> params = new HashMap<>();
-        params.put("userId", "20");
-        params.put("remark", "用户Id变为了20");
-        params.put("testCommand", "pwd");
+        // Json字符串转成Map<String, String>的方式
+        String command = "{\"userId\":20,\"remark\":\"用户Id变为了20\",\"testCommand\":\"pwd\"}";
+        Map<String, String> params = new ObjectMapper().readValue(command, new TypeReference<Map<String, String>>() {
+        });
+        System.out.println(params);
+//        Map<String, String> params = new HashMap<>();
+//        params.put("userId", "20");
+//        params.put("remark", "用户Id变为了20");
+//        params.put("testCommand", "pwd");
         jenkinsInfo.setCommand(params);
 
         JenkinsClient jenkinsClient = new JenkinsClient();
